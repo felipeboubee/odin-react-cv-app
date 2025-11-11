e3f84ddba527edea4d187271b5f22cae286a62f0
Felipe <230595010+felipeboubee@users.noreply.github.com>
2025-11-11 17:39:51 -0300

    Added base components

---
+++ src/components/Education.jsx
@@ -0,0 +1,14 @@
+function Education(){
+    return (<form action="post">
+        <label htmlFor="">Full Name</label>
+        <input type="text" />
+        <label htmlFor="">Email</label>
+        <input type="email" />
+        <label htmlFor="">Phone Number</label>
+        <input type="tel" />
+        <label htmlFor="">Address</label>
+        <input type="text" />
+    </form>)
+}
+
+export default Education;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+function Sidebar (){
+    return (
+        <nav className="sidebar">
+            <button className="content">Content</button>
+            <button className="design">Design</button>
+            <button className="clear">Clear Resume</button>
+        </nav>
+    )
+};
+
+export default Sidebar;
