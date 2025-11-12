8944466ea50b58f81a21c90633d5cf741fe41b15
Felipe <230595010+felipeboubee@users.noreply.github.com>
2025-11-12 11:41:10 -0300

    Added functions for reusable components
    InputGroup and Section are now reusable blocks

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,4 +1,15 @@
-export default function InputGroup(){
+export default function InputGroup({id, label, type, value, onChange, placeholder}){
+    return (
+        <div className="input-group">
+            <label htmlFor={id}>{label}</label>
+            <input 
+            type={type} 
+            value={value} 
+            placeholder={placeholder}
+            onChange={((e) => onChange(e.target.value))}    // Pass the updated value upward
+            />
+        </div>
+    )
     
 }
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,3 +1,8 @@
-export default function Section(){
-    
+export default function Section({title, children}){
+    return (
+    <section>
+        <h2 className="section-title">{title}</h2>
+        <div className="section-content">{children}</div>
+    </section>
+    );
 }
