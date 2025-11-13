5559d78cd71b8bdce42573e5a222f15999864920
Felipe <230595010+felipeboubee@users.noreply.github.com>
2025-11-13 15:38:34 -0300

    Added code for Education section

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import Sidebar from './components/Sidebar.jsx'
 import PersonalDetails from './components/sections/PersonalDetails.jsx'
 import Section from './components/Section.jsx'
-/* import Education from './components/sections/Education.jsx'
-import Experience from './components/sections/Experience.jsx'
+import Education from './components/sections/Education.jsx'
+/* import Experience from './components/sections/Experience.jsx'
 import Display from './components/Display.jsx' */
 
 function App() {
@@ -15,7 +15,8 @@ function App() {
       email: "",
       phone: "",
       address: "",
-    }
+    },
+    education: [],
   })
 
   function updateSection(sectionName, newData){
@@ -33,6 +34,12 @@ function App() {
       <PersonalDetails
         data={cvData.personal}
         onChange={(data) => updateSection("personal", data)}
+      />
+    </Section>
+    <Section title="Education">
+      <Education
+        data={cvData.education}
+        onChange={(data) => updateSection("education", data)}
       />
     </Section>
   </div>
--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -1,14 +1,66 @@
-function Education(){
-    return (<form action="post">
-        <label htmlFor="">Full Name</label>
-        <input type="text" />
-        <label htmlFor="">Email</label>
-        <input type="email" />
-        <label htmlFor="">Phone Number</label>
-        <input type="tel" />
-        <label htmlFor="">Address</label>
-        <input type="text" />
-    </form>)
+import InputGroup from '../InputGroup.jsx'
+
+export default function Education({data, onChange}){
+    
+    function addEducation(){
+        onChange([...data,{school: "", degree: "", startDate: "", endDate: "", location: ""}]);
+    }
+
+    function updateEducation(index, field, value){
+        const updated = [...data];
+        updated[index][field] = value;
+        onChange(updated);
+    }
+
+    return (
+        <div>
+            {data.map((entry, index) => (
+                <div key={index} className='education-entry'>
+                    <InputGroup
+                        type="text"
+                        id="school"
+                        label="School Name"
+                        value={entry.school}
+                        placeholder="School Name"
+                        onChange={((v) => updateEducation(index, "school", v))}
+                    />
+                    <InputGroup
+                        type="text"
+                        id="degree"
+                        label="Degree"
+                        value={entry.degree}
+                        placeholder="Degree"
+                        onChange={((v) => updateEducation(index, "degree", v))}
+                    />
+                    <InputGroup
+                        type="date"
+                        id="startDate"
+                        label="Start Date"
+                        value={entry.startDate}
+                        placeholder=""
+                        onChange={((v) => updateEducation(index, "startDate", v))}
+                    />
+                    <InputGroup
+                        type="date"
+                        id="endDate"
+                        label="End Date"
+                        value={entry.endDate}
+                        placeholder=""
+                        onChange={((v) => updateEducation(index, "endDate", v))}
+                    />
+                    <InputGroup
+                        type="text"
+                        id="location"
+                        label="Location"
+                        value={entry.location}
+                        placeholder="Location"
+                        onChange={((v) => updateEducation(index, "location", v))}
+                    />
+
+                </div>
+            ))}
+            <button onClick={addEducation}>Add Education</button>
+        </div>
+    )
 }
 
-export default Education;
